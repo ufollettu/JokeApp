08d0d44752fae55f939e3d1dcf2bb6253e6ed0a6
Pasquale Merolle <pasquale.merolle@gmail.com>
2018-05-15 12:00:32 +0200

    edited searchform to search live with observable and switch

--- src/app/itunes-search/itunes-search.component.ts
+++ src/app/itunes-search/itunes-search.component.ts
@@ -2,6 +2,11 @@ import {Component, OnInit} from '@angular/core';
 import {SearchServiceService} from "../search-service.service";
 import {SearchItem} from "../search-item";
 import {Observable} from "rxjs/Observable";
+import {FormControl} from "@angular/forms";
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/do';
 
 @Component({
   selector: 'app-itunes-search',
@@ -12,11 +17,20 @@ export class ItunesSearchComponent implements OnInit {
   private loading:boolean = false;
   // private results: SearchItem[]; // used by observable method
   private results: Observable<SearchItem[]>; // used by async pipe + obs
+  private searchField: FormControl;
 
   constructor(private itunes: SearchServiceService) {
   }
 
   ngOnInit() {
+    this.searchField = new FormControl();
+    this.results = this.searchField.valueChanges
+      .debounceTime(400)
+      .distinctUntilChanged()
+      .do(() => this.loading = true)
+      .switchMap(term => this.itunes.search(term))
+      .do(() => this.loading = false); // change from Observable<Observable<SearchItem[]>> into Observable<SearchItem[]>
+      // .subscribe(value => console.log(value)); // Need to call subscribe to make it hot!
   }
 
   // search in iTunes db
@@ -47,8 +61,7 @@ export class ItunesSearchComponent implements OnInit {
   //
   doSearch(term: string) {
     this.loading = true;
-    this.results = this.itunes.search(term)
-      .do(() => this.loading = false);
+    this.results = this.itunes.search(term);
   }
 
 }
